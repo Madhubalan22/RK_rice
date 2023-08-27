0b5b9a031f33ca0042662f4640a140d801634fc8
Lokesh Kumar <lokeshkumarv2002@gmail.com>
2023-08-27 13:35:49 +0530

    sample nav

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <header>
+        <h3>logo</h3>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Services</a>
+            <a href="#">Contact</a>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
