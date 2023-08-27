be84990ac12e5fe6b7c35aed5ab61e64c3a334d4
Lokesh Kumar <lokeshkumarv2002@gmail.com>
2023-08-27 14:32:13 +0530

    nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,32 @@
 import React from 'react'
+import { useRef } from 'react';
+import {FaBars,FaTimes} from "react-icons/fa";
+import "../styles/nav.css";
 
 function Nav() {
+
+    const navRef=useRef();
+
+    const showNavbar=()=>{
+        navRef.current.classList.toggle("responsive_nav");
+    }
+
   return (
+    
     <header>
-        <h3>logo</h3>
-        <nav>
+        <img src="src\images\logomerged.png" />
+        <nav ref={navRef}>
             <a href="#">Home</a>
             <a href="#">Services</a>
+            <a href="#">About</a>
             <a href="#">Contact</a>
+            <button  className="nav-btn nav-close-btn" onClick={showNavbar}>  
+               <FaTimes/>
+            </button>
         </nav>
+        <button className="nav-btn" onClick={showNavbar}>
+              <FaBars/>
+        </button>
     </header>
   )
 }
