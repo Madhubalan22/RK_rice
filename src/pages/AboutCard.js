c2f7b0b4e3b803dde56e72fb4d1bd504171bfa5a
Lokesh Kumar <lokeshkumarv2002@gmail.com>
2023-08-28 18:00:38 +0530

    about

---
+++ src/pages/AboutCard.js
@@ -0,0 +1,39 @@
+import React from "react"
+import "../styles/about.css"
+import { homeAbout } from "../pages/dummydata"
+import Awrapper from "./Awrapper"
+
+const AboutCard = () => {
+  return (
+    <>
+      <section className='aboutHome'>
+        <div className='container flexSB'>
+          <div className='left row'>
+            <img src='./images/about.webp' alt='' />
+          </div>
+          <div className='right row'>
+            <h1>Rice mill about us</h1>
+            <div className='items'>
+              {homeAbout.map((val) => {
+                return (
+                  <div className='item flexSB'>
+                    <div className='img'>
+                      <img src={val.cover} alt='' />
+                    </div>
+                    <div className='text'>
+                      <h2>{val.title}</h2>
+                      <p>{val.desc}</p>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
+      <Awrapper />
+    </>
+  )
+}
+
+export default AboutCard
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Aboutcard from './AboutCard'
 
 const about = () => {
   return (
-    <div>about</div>
+    <div>
+       <Aboutcard/>
+    </div>
   )
 }
 
