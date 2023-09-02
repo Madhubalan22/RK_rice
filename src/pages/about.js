338961b1b1aa0facf491b75fced25691af7ada63
Logith <logithramesh4444@gmail.com>
2023-09-02 23:40:03 +0530

    log

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import "../styles/about.css"
-
+import Nav from '../components/Nav.js'
 const about = () => {
   return (
+    
     <div>
-       about
+      <Nav/>
+      <div className='about1'>
+       <h1 className='about'>ABOUT US</h1>
+       <p className='para'>
+       At RK Rice Mill, our passion for perfection in rice processing has been the cornerstone of our success for decades. As a trusted name in the industry, we have consistently delivered premium quality rice products that have become a staple in households around the world. Our commitment to excellence, paired with state-of-the-art technology, has made us a leader in the field of rice processing.
+       </p>
+       <div className='rect'></div>
+       <div className='rect1'></div>
+       </div>
+
+       <div class="container">
+        <div class="half">Left Half</div>
+        <div class="half1">Right Half</div>
+    </div>
+
     </div>
   )
 }
